eab5bb14a5014cb140c9ba81350853512741f350
Yemi <fbabajide@deakin.edu.au>
2020-05-26 02:16:28 +1000

    continuous development setup
    Signed-off-by: Yemi <fbabajide@deakin.edu.au>

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express')
 var app= express()
 
-var port=3000;
+//var port=3000;
+var port = process.env.PORT || 8080;
 
 
 app.use(express.static(__dirname+"/public"));
 app.listen(port)
 console.log('server listening on port: ' + port)
 
+
+require("cf-deployment-tracker-client").track();
