a96460b7218dfbb1000ae66cde0641c87eabe698
Yemi <fbabajide@deakin.edu.au>
2020-05-26 20:56:25 +1000

    IBM Cloud Upload
    Signed-off-by: Yemi <fbabajide@deakin.edu.au>

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app= express()
+var express = require('express');
+var app= express();
 
 
 var port = process.env.PORT || 8080;
 
 
 app.use(express.static(__dirname+"/public"));
-app.listen(port)
-console.log('server listening on port: ' + port)
+app.listen(port);
+console.log('server listening on port: ' + port);
 
 
-require("cf-deployment-tracker-client").track();
+//require("cf-deployment-tracker-client").track();
